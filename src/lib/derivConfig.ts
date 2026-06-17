4ff5dc9e92c5f05c59bfb2f13032a154f70b0d9a
mihiretuNEW <mirelidolij@gmail.com>
2026-06-17 14:06:50 +0300

    Update derivConfig.ts

--- src/lib/derivConfig.ts
+++ src/lib/derivConfig.ts
@@ -1,5 +1,5 @@
 export const DERIV_APP_ID = 1089; // Standard open app_id for Deriv
-export const DERIV_WS_URL = `wss://ws.binaryws.com/websockets/v3?app_id=${DERIV_APP_ID}`;
+export const DERIV_WS_URL = `wss://ws.derivws.com/websockets/v3?app_id=${DERIV_APP_ID}`;
 
 export const ASSET_PAIRS = [
   // Forex
